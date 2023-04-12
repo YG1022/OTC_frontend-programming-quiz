def25e150ae50be5fac4d3c64fb075cb9f42f3da
YG1022 <958065532@qq.com>
2023-04-12 22:40:18 +0800

    feat: init custom footer component

---
+++ src/components/customFooter.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="my-footer">
+    <div class="total-amount">总计:¥ 100</div>
+    <button type="button" class="footer-btn btn btn-primary">取消</button>
+    <button type="button" class="footer-btn btn btn-primary">提交</button>
+  </div>
+</template>
