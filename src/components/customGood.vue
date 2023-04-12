1fd35b6d0edcf8ac7b4c5e8220c3322e5c97d2a3
YG1022 <958065532@qq.com>
2023-04-12 22:02:11 +0800

    feat: init custom good component

---
+++ src/components/customGood.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="my-goods-item">
+    <div class="left">
+      <div class="custom-control custom-checkbox">
+        <label class="custom-control-label" for="input">
+          <img
+            src="http://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg"
+            alt=""
+          />
+        </label>
+      </div>
+    </div>
+    <div class="right">
+      <div class="top">商品名称</div>
+      <div class="bottom">
+        <span class="price">单价: ¥ 100</span>
+        <span class="stock">剩余库存: 0</span>
+        <span> 数量组件 </span>
+      </div>
+    </div>
+  </div>
+</template>
+<script></script>
+<style></style>
