8afa25b124974905d126d03c615650899df86e77
YG1022 <958065532@qq.com>
2023-04-12 22:34:43 +0800

    feat: init custom count component

---
+++ src/components/customCount.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="my-counter">
+    <button class="btn btn-light">-</button>
+    <div class="amount">商品数量</div>
+    <button class="btn btn-light">+</button>
+  </div>
+</template>
+<script setup></script>
+<style></style>
