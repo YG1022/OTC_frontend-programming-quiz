b46c198906b1cb7408e68ffe56211f48d43b7627
YG1022 <958065532@qq.com>
2023-04-13 09:55:32 +0800

    feat: import custom good

--- src/views/order-create/index.vue
+++ src/views/order-create/index.vue
@@ -1,12 +1,42 @@
 <template>
-  <h1>OrderCreatePage</h1>
+  <main>
+    <h1>Order Create</h1>
+    <div class="goods">
+      <custom-good v-for="good in goods" :key="good.id" :good="good" />
+    </div>
+  </main>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, onMounted, ref } from "vue";
+import CustomGood from "@/components/custom-good/index.vue";
+
+interface good {
+  id: string;
+  name: string;
+  description: string;
+  cover: string;
+  stock: number;
+  price: number;
+}
 
 export default defineComponent({
   name: "OrderCreatePage",
+  components: {
+    CustomGood,
+  },
+
+  setup() {
+    const goods = ref<good[]>([]);
+
+    onMounted(() => {
+      fetch("/api/goods", { method: "GET" })
+        .then((res) => res.json())
+        .then((data) => (goods.value = data));
+    });
+
+    return { goods };
+  },
 });
 </script>
 
