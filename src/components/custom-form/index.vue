c55d71c6851943dbd5cbd5f65b4b326421367790
YG1022 <958065532@qq.com>
2023-04-13 20:20:46 +0800

    feat: realise cascade selector

--- src/components/custom-form/index.vue
+++ src/components/custom-form/index.vue
@@ -7,13 +7,29 @@
       <el-input v-model="form.nickName"></el-input>
     </el-form-item>
     <el-form-item label="手机号码" prop="phone">
-      <el-select v-model="form.prefix"></el-select>
+      <select v-model="form.prefix">
+        <option v-for="prefix in prefixes" :value="prefix" :key="prefix">
+          {{ prefix }}
+        </option>
+      </select>
       <el-input v-model="form.phone"> </el-input>
     </el-form-item>
     <el-form-item label="收货地址" prop="address">
-      <el-select v-model="form.province"></el-select>
-      <el-select v-model="form.city"></el-select>
-      <el-select v-model="form.discrict"></el-select>
+      <select v-model="form.province" @change="updateCities">
+        <option v-for="province in provinces" :value="province" :key="province">
+          {{ province }}
+        </option>
+      </select>
+      <select v-model="form.city" @change="updateDistricts">
+        <option v-for="city in cities" :value="city" :key="city">
+          {{ city }}
+        </option>
+      </select>
+      <select v-model="form.discrict">
+        <option v-for="district in districts" :value="district" :key="district">
+          {{ district }}
+        </option>
+      </select>
       <el-input v-model="form.unit"></el-input>
     </el-form-item>
     <label>选择商品</label>
@@ -47,7 +63,51 @@ export default defineComponent({
 
     return {
       form: initialFormData,
+      prefixes: ["+86", "+852", "+853", "+886"],
+      provinces: ["Province A", "Province B", "Province C"],
+      cities: [] as string[],
+      districts: [] as string[],
     };
+  },
+  methods: {
+    updateCities() {
+      if (this.form.province === "Province A") {
+        this.cities = ["City A1", "City A2", "City A3"];
+      } else if (this.form.province === "Province B") {
+        this.cities = ["City B1", "City B2", "City B3"];
+      } else if (this.form.province === "Province C") {
+        this.cities = ["City C1", "City C2", "City C3"];
+      } else {
+        this.cities = [];
+      }
+      this.form.city = "";
+      this.form.discrict = "";
+      this.districts = [];
+    },
+    updateDistricts() {
+      if (this.form.city === "City A1") {
+        this.districts = ["District A1-1", "District A1-2", "District A1-3"];
+      } else if (this.form.city === "City A2") {
+        this.districts = ["District A2-1", "District A2-2", "District A2-3"];
+      } else if (this.form.city === "City A3") {
+        this.districts = ["District A3-1", "District A3-2", "District A3-3"];
+      } else if (this.form.city === "City B1") {
+        this.districts = ["District B1-1", "District B1-2", "District B1-3"];
+      } else if (this.form.city === "City B2") {
+        this.districts = ["District B2-1", "District B2-2", "District B2-3"];
+      } else if (this.form.city === "City B3") {
+        this.districts = ["District B3-1", "District B3-2", "District B3-3"];
+      } else if (this.form.city === "City C1") {
+        this.districts = ["District C1-1", "District C1-2", "District C1-3"];
+      } else if (this.form.city === "City C2") {
+        this.districts = ["District C2-1", "District C2-2", "District C2-3"];
+      } else if (this.form.city === "City C3") {
+        this.districts = ["District C3-1", "District C3-2", "District C3-3"];
+      } else {
+        this.districts = [];
+      }
+      this.form.discrict = "";
+    },
   },
 });
 </script>
