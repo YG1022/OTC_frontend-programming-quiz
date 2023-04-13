c0207e092f3976ca4629ce8d333287e851cb10ab
YG1022 <958065532@qq.com>
2023-04-13 16:34:59 +0800

    feat: a router link to create order page

--- src/views/order-list/index.vue
+++ src/views/order-list/index.vue
@@ -1,15 +1,22 @@
 <template>
   <main>
-    <header class="header fs-20">订单列表</header>
+    <header class="header fs-20">
+      订单列表
+      <RouterLink to="/create">
+        <button type="button" class="btn btn-primary">新增订单</button>
+      </RouterLink>
+    </header>
     <p class="description fs-20">请在此项目基础上完成 README 中的任务</p>
   </main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import { RouterLink } from "vue-router";
 
 export default defineComponent({
   name: "OrderListPage",
+  components: { RouterLink },
 });
 </script>
 
