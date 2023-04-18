8f9b41e491abfa615d24cffc94df10045e8f9da8
YG1022 <958065532@qq.com>
2023-04-18 13:49:14 +0800

    refactor: use setup to replace options API in count component

--- src/components/custom-count/index.vue
+++ src/components/custom-count/index.vue
@@ -8,54 +8,98 @@
 
 <script lang="ts">
 import type { good } from "@/constants/type";
-import { defineComponent } from "vue";
+import { defineComponent, onBeforeUnmount, ref, watch } from "vue";
 import { goodsStore } from "@/store/store";
 
 export default defineComponent({
-  name: "CustomCount",
   props: {
     obj: {
       type: Object as () => good,
       required: true,
     },
   },
-  data() {
-    return {
-      goodObj: this.obj,
-      maxQuantity: this.obj.stock,
+  setup(props) {
+    const goodObj = ref(props.obj);
+    const maxQuantity = ref(props.obj.stock);
+    const handleAdd = () => {
+      if (goodObj.value.amount < goodObj.value.stock) {
+        goodObj.value.amount++;
+        goodsStore.updateGood(goodObj.value);
+      }
     };
-  },
-  methods: {
-    handleAdd() {
-      if (this.goodObj.amount < this.goodObj.stock) {
-        this.goodObj.amount++;
-        goodsStore.updateGood(this.goodObj);
+    const handleSub = () => {
+      if (goodObj.value.amount > 0) {
+        goodObj.value.amount--;
+        goodsStore.updateGood(goodObj.value);
       }
-    },
-    handleSub() {
-      if (this.goodObj.amount > 0) {
-        this.goodObj.amount--;
-        goodsStore.updateGood(this.goodObj);
-      }
-    },
-  },
-  watch: {
-    goodObj: {
-      handler(newValue) {
-        if (newValue.amount > this.maxQuantity) {
-          this.goodObj.amount = this.maxQuantity;
+    };
+    watch(
+      goodObj,
+      (newValue) => {
+        if (newValue.amount > maxQuantity.value) {
+          goodObj.value.amount = maxQuantity.value;
         } else if (newValue.amount < 0) {
-          this.goodObj.amount = 0;
+          goodObj.value.amount = 0;
         }
       },
-      deep: true,
-    },
-  },
-  beforeDestroy() {
-    this.goodObj.amount = 0;
-    goodsStore.updateGood(this.goodObj);
+      { deep: true }
+    );
+    onBeforeUnmount(() => {
+      goodObj.value.amount = 0;
+      goodsStore.updateGood(goodObj.value);
+    });
+    return {
+      goodObj,
+      handleAdd,
+      handleSub,
+    };
   },
 });
+// export default defineComponent({
+//   name: "CustomCount",
+//   props: {
+//     obj: {
+//       type: Object as () => good,
+//       required: true,
+//     },
+//   },
+//   data() {
+//     return {
+//       goodObj: this.obj,
+//       maxQuantity: this.obj.stock,
+//     };
+//   },
+//   methods: {
+//     handleAdd() {
+//       if (this.goodObj.amount < this.goodObj.stock) {
+//         this.goodObj.amount++;
+//         goodsStore.updateGood(this.goodObj);
+//       }
+//     },
+//     handleSub() {
+//       if (this.goodObj.amount > 0) {
+//         this.goodObj.amount--;
+//         goodsStore.updateGood(this.goodObj);
+//       }
+//     },
+//   },
+//   watch: {
+//     goodObj: {
+//       handler(newValue) {
+//         if (newValue.amount > this.maxQuantity) {
+//           this.goodObj.amount = this.maxQuantity;
+//         } else if (newValue.amount < 0) {
+//           this.goodObj.amount = 0;
+//         }
+//       },
+//       deep: true,
+//     },
+//   },
+//   beforeDestroy() {
+//     this.goodObj.amount = 0;
+//     goodsStore.updateGood(this.goodObj);
+//   },
+// });
 </script>
 
 <style scoped>
@@ -66,12 +110,12 @@ export default defineComponent({
   outline: none;
   cursor: pointer;
 }
+
 .count {
   width: 80px;
   height: 40px;
   padding: 0;
   margin: 0 5px;
-  padding: 0;
   outline: none;
   text-align: center;
 }
