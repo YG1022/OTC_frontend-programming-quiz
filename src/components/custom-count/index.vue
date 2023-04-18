764edb71e5f63051afef905caa098ef812fe9ebf
YG1022 <958065532@qq.com>
2023-04-18 13:49:52 +0800

    refactor: remove unused comments

--- src/components/custom-count/index.vue
+++ src/components/custom-count/index.vue
@@ -55,51 +55,6 @@ export default defineComponent({
     };
   },
 });
-// export default defineComponent({
-//   name: "CustomCount",
-//   props: {
-//     obj: {
-//       type: Object as () => good,
-//       required: true,
-//     },
-//   },
-//   data() {
-//     return {
-//       goodObj: this.obj,
-//       maxQuantity: this.obj.stock,
-//     };
-//   },
-//   methods: {
-//     handleAdd() {
-//       if (this.goodObj.amount < this.goodObj.stock) {
-//         this.goodObj.amount++;
-//         goodsStore.updateGood(this.goodObj);
-//       }
-//     },
-//     handleSub() {
-//       if (this.goodObj.amount > 0) {
-//         this.goodObj.amount--;
-//         goodsStore.updateGood(this.goodObj);
-//       }
-//     },
-//   },
-//   watch: {
-//     goodObj: {
-//       handler(newValue) {
-//         if (newValue.amount > this.maxQuantity) {
-//           this.goodObj.amount = this.maxQuantity;
-//         } else if (newValue.amount < 0) {
-//           this.goodObj.amount = 0;
-//         }
-//       },
-//       deep: true,
-//     },
-//   },
-//   beforeDestroy() {
-//     this.goodObj.amount = 0;
-//     goodsStore.updateGood(this.goodObj);
-//   },
-// });
 </script>
 
 <style scoped>
